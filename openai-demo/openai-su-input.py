bdb6731a5827ee36bceb6e73ffe43e459d5a7266
Germis2021 <89936809+Germis2021@users.noreply.github.com>
2025-05-25 16:15:07 +0300

    openai su input atsakymai Lietuviskai, contextas ir breakas

---
+++ openai-demo/openai-su-input.py
@@ -0,0 +1,51 @@
+#You will practice Openai Python client usage:
+# -- Advanced, optional
+# 8. Update this script to be a console application, 
+# where you can enter a question and AI would give you an answer.
+# 9. Force AI to always answer in Lithuanian language.
+# 10. If 'exit' command is given, exit the application instead of calling ai.
+# 11. Whole conversation should be taken into context.
+
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()  # Paimamos reikšmės iš .env failo
+
+token = os.getenv("SECRET")
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-4.1-nano"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
+
+print("Uzduok savo klausima (Jei nori išeiti spausk'exit' arba 'quit' ):")
+
+while True:
+    user_question = input()
+
+    if user_question.lower() in ["exit", "quit"]:
+        print("Iki!")
+        break
+
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant.",
+            },
+            {
+                "role": "user",
+                "content": user_question + " (Please answer in Lithuanian.)",
+            }
+        ],
+        temperature=1.0,
+        top_p=1.0,
+        model=model
+    )
+
+    print("Asistentas:", response.choices[0].message.content)
+
+
